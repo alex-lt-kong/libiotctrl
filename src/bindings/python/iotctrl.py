5192fd5eb36eaaea0b9c9e7df04977741864ef36
Alex Kong <alex.lt.kong@connect.ust.hk>
2022-12-26 18:10:49 +0800

    python docstring

--- src/bindings/python/iotctrl.py
+++ src/bindings/python/iotctrl.py
@@ -12,6 +12,21 @@ iotctrl.get_temperature.argtypes = [ct.c_char_p, ct.c_int]
 iotctrl.get_temperature.restype = ct.c_int
 
 def get_temperature(device_path: str, debug_mode: int) -> int:
+    """
+        Get temperature in degree celsius from sensor
+
+        Parameters
+        ----------
+        device_path : str
+            The path of the temperature sensor, typically /dev/ttyUSB0
+        debug_mode : int
+            Pass 1 to enable debug mode, i.e., diagnostic information will be printed to stdout
+
+        Returns
+        -------
+        the temperature reading in degree celsius
+    """
+
     temp = iotctrl.get_temperature(ct.create_string_buffer(str.encode(device_path)), debug_mode)
     if temp == INVALID_TEMP:
         raise RuntimeError('Failed to read temperature (some error messages could be sent to stderr)')
@@ -19,6 +34,20 @@ def get_temperature(device_path: str, debug_mode: int) -> int:
 
 
 def set_relay(relay_path: str, state: bool) -> None:
+    """
+        Turn a relay on or off
+
+        Parameters
+        ----------
+        relay_path : str
+            The path of the relay, typically /dev/ttyUSB0
+        state : int
+            True to turn it on; false to turn it off.
+
+        Returns
+        -------
+        None
+    """
     turn_on = ct.c_bool()
     turn_on.value = state
     return iotctrl.control_relay(ct.create_string_buffer(str.encode(relay_path)), turn_on)
