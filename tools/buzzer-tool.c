612d237612d2225f2cf21e6e5ec5ac1c02921112
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-11-04 14:52:06 +0000

    refactoring

--- tools/buzzer-tool.c
+++ tools/buzzer-tool.c
@@ -8,34 +8,18 @@
 #include <sys/file.h>
 #include <unistd.h>
 
-#define LOCK_FILE_PATH "/tmp/beeper.lck"
+
 #define SIGNAL_PIN 4
 
 int main() {
-  int ret, fd;
+  int ret;
 
-  fd = open(LOCK_FILE_PATH, O_RDWR | O_CREAT, 0644);
-  if (fd < 0) {
-    perror("Failed to open lock file");
-    exit(1);
-  }
-
-  ret = flock(fd, LOCK_EX | LOCK_NB);
-  if (ret != 0) {
-    printf("File already locked\n");
-    return -1;
-  }
-
-  printf("File available, not locked\n");
-  /*struct timespec sequence[] = {{1, 0}, {1, 0}, {1, 0}, {1, 0}, {1, 0},
-                                {1, 0}, {1, 0}, {1, 0}, {1, 0}};*/
-  struct beepUnit sequence[] = {{1, 100}, {0, 100}, {1, 100}, {0, 2000},
+  struct buzz_unit sequence[] = {{1, 100}, {0, 100}, {1, 100}, {0, 2000},
                                 {1, 100}, {0, 100}, {1, 100}, {0, 2000}};
   size_t length = sizeof(sequence) / sizeof(sequence[0]);
 
   beep(0, 4, sequence, length);
-  flock(fd, LOCK_UN);
-  close(fd);
+
 
   return 0;
 }
