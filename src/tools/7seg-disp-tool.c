7ea8bd7cebd2967ba345fb10d10058edb5cb2359
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-11-12 00:09:12 +0800

    allow users to specify data pin

--- src/tools/7seg-disp-tool.c
+++ src/tools/7seg-disp-tool.c
@@ -12,31 +12,37 @@
 void print_help_then_exit(char **argv) {
   // clang-format off
   printf("Usage: %s\n"
-         "    -d, --device-path <device_path>   The path of the GPIO device, typically something like /dev/gpiochip0\n",
+         "    -p, --device-path <device_path>   The path of the GPIO device, typically something like /dev/gpiochip0\n"
+         "    -d, --data-pin    <pin_number>    The GPIO pin number in GPIO/BCM schema that connects to the DIO pin\n",
          argv[0]);
   // clang-format on
   _exit(0);
 }
 
-void parse_arguments(int argc, char **argv, char **device_path) {
+void parse_arguments(int argc, char **argv, char **device_path,
+                     ssize_t *data_pin) {
   int c;
   // https://www.gnu.org/software/libc/manual/html_node/Getopt-Long-Option-Example.html
   while (1) {
     static struct option long_options[] = {
-        {"device-path", required_argument, 0, 'd'},
+        {"device-path", required_argument, 0, 'p'},
+        {"data-pin", required_argument, 0, 'd'},
         {"help", no_argument, 0, 'h'},
         {NULL, 0, NULL, 0}};
     /* getopt_long stores the option index here. */
     int option_index = 0;
 
-    c = getopt_long(argc, argv, "d:h", long_options, &option_index);
+    c = getopt_long(argc, argv, "p:d:h", long_options, &option_index);
 
     /* Detect the end of the options. */
     if (c == -1)
       break;
     switch (c) {
-    case 'd':
+    case 'p':
       *device_path = optarg;
+      break;
+    case 'd':
+      *data_pin = atoi(optarg);
       break;
     case 'h':
       print_help_then_exit(argv);
@@ -45,16 +51,17 @@ void parse_arguments(int argc, char **argv, char **device_path) {
       print_help_then_exit(argv);
     }
   }
-  if (*device_path == NULL) {
+  if (*device_path == NULL || *data_pin <= 0) {
     print_help_then_exit(argv);
   }
 }
 
 int main(int argc, char **argv) {
   char *gpio_device_path = NULL;
-  parse_arguments(argc, argv, &gpio_device_path);
+  ssize_t data_pin = -1;
+  parse_arguments(argc, argv, &gpio_device_path, &data_pin);
   struct iotctrl_7seg_display_connection conn = {.display_digit_count = 8,
-                                                 .data_pin_num = 17,
+                                                 .data_pin_num = data_pin,
                                                  .clock_pin_num = 11,
                                                  .latch_pin_num = 18,
                                                  .chain_num = 2};
