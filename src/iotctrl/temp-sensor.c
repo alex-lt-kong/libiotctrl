4b36cf2ddd769ea4351181d21f5377fb3a7fdbfa
Alex Kong <alex.lt.kong@connect.ust.hk>
2023-11-12 17:11:58 +0800

    robustness improvements

--- src/iotctrl/temp-sensor.c
+++ src/iotctrl/temp-sensor.c
@@ -33,21 +33,27 @@ int iotctrl_get_temperature(const char *sensor_path, uint8_t sensor_count,
                             int16_t *readings, const int enable_debug_output) {
   int ret = 0;
   modbus_t *mb_ctx = NULL;
+
   mb_ctx = modbus_new_rtu(sensor_path, 9600, 'N', 8, 1);
   if (mb_ctx == NULL) {
-    fprintf(stderr, "Unable to create the libmodbus context\n");
+    fprintf(stderr, "modbus_new_rtu() failed: %s\n", modbus_strerror(errno));
     ret = -1;
     goto finally;
   }
 
-  modbus_set_slave(mb_ctx, 1);
-  modbus_set_debug(mb_ctx, enable_debug_output == 1);
-  if (modbus_connect(mb_ctx) == -1) {
-    fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+  if (modbus_set_slave(mb_ctx, 1) != 0) {
+    fprintf(stderr, "modbus_set_slave() failed: %s\n", modbus_strerror(errno));
     ret = -2;
     goto finally;
   }
-  uint8_t raw_req[] = {0x01, 0x04, 0x04, 0x00, 0x00, sensor_count};
+  (void)modbus_set_debug(mb_ctx, enable_debug_output == 1);
+  if (modbus_connect(mb_ctx) != 0) {
+    fprintf(stderr, "modbus_connect() failed: %s\n", modbus_strerror(errno));
+    ret = -3;
+    goto finally;
+  }
+
+  const uint8_t raw_req[] = {0x01, 0x04, 0x04, 0x00, 0x00, sensor_count};
   // clang-format off
   // Note that we have to truncate the bytes series from 8 to 6 to make it work.
   // Page 12 of the manufacturer manual documents the format of command bytes format:
@@ -60,16 +66,16 @@ int iotctrl_get_temperature(const char *sensor_path, uint8_t sensor_count,
 
   uint8_t rsp[MODBUS_RTU_MAX_ADU_LENGTH];
 
-  int req_length = modbus_send_raw_request(
+  const int req_length = modbus_send_raw_request(
       mb_ctx, raw_req, sizeof(raw_req) / sizeof(raw_req[0]));
   if (req_length == -1) {
-    fprintf(stderr, "Failed to send raw request: %s\n", modbus_strerror(errno));
+    fprintf(stderr, "modbus_send_raw_request() failed: %s\n",
+            modbus_strerror(errno));
     ret = -3;
     goto finally;
   }
-  int rc = modbus_receive_confirmation(mb_ctx, rsp);
-  if (rc == -1) {
-    fprintf(stderr, "Failed to receive a confirmation request: %s\n",
+  if (modbus_receive_confirmation(mb_ctx, rsp) != 0) {
+    fprintf(stderr, "modbus_receive_confirmation() failed: %s\n",
             modbus_strerror(errno));
     ret = -4;
     goto finally;
@@ -84,21 +90,21 @@ int iotctrl_get_temperature(const char *sensor_path, uint8_t sensor_count,
   // clang-format on
 
   if (rsp[0] == 0x01 || rsp[1] == 0x04 || rsp[2] == 0x02) {
+    const uint16_t calculated_crc = calculate_crc16(rsp, 3 + sensor_count * 2);
+    const uint16_t expected_crc =
+        (rsp[4 + sensor_count * 2] << 8) + rsp[3 + sensor_count * 2];
+    if (calculated_crc != expected_crc) {
+      fprintf(stderr, "CRC value does not match!\n");
+      ret = -5;
+      goto finally;
+    }
     for (uint8_t i = 0; i < sensor_count; ++i) {
-      readings[i] = ((rsp[3 + i * 2] << 8) + rsp[4 + i * 2]);
+      readings[i] = (rsp[3 + i * 2] << 8) + rsp[4 + i * 2];
       if (readings[i] == IOTCTRL_INVALID_TEMP) {
         fprintf(stderr,
                 "Sensor no. %u (index from 0) returns is INVALID_TEMP(%d). The "
                 "sensor might be non-existent or malfunctional\n",
                 i + 1, IOTCTRL_INVALID_TEMP);
-        ret = -5;
-        goto finally;
-      }
-      uint16_t calculated_crc = calculate_crc16(rsp, 3 + sensor_count * 2);
-      uint16_t expected_crc =
-          (rsp[4 + sensor_count * 2] << 8) + rsp[3 + sensor_count * 2];
-      if (calculated_crc != expected_crc) {
-        fprintf(stderr, "CRC value does not match!\n");
         ret = -6;
         goto finally;
       }
